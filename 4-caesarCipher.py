6af91a1b4cd4686b1e9151bbfbd3333cd99dba7d
chibokamc <chibokachinedum@gmail.com>
2023-01-18 00:25:43 +0100

    Completed encode and decode features

--- 4-caesarCipher.py
+++ 4-caesarCipher.py
@@ -2,19 +2,30 @@ import string
 a = string.ascii_lowercase
 print(a)
 alphabet = [*a]
-print(alphabet)
-print(alphabet)
+print(alphabet + alphabet)
 
-encode_decode = input("Type 'encode' or 'decode':\n")
+
 message = input("Type your message:\n").lower()
 shift_value = int(input("Enter a shift value:\n"))
+encode_decode = input("Type 'encode' or 'decode':\n").lower()
 
+if(encode_decode == 'encode'):
+  def encrypt(text, shift):
+    code=""
+    for letter in message:
+      idx = alphabet.index(letter)
+      code += alphabet[idx + shift]
+    print(code)
+  encrypt(message, shift_value)
 
-def encrypt(text, shift):
-  code=""
-  for letter in message:
-    idx = alphabet.index(letter)
-    # letter_code = alphabet[idx + shift]
-    code += alphabet[idx + shift]
-  print(code)
-encrypt('hello', 5)
+elif(encode_decode == 'decode'):
+  def decrypt(text, shift):
+    decoded=""
+    for letter in message:
+      idx = alphabet.index(letter)
+      decoded += alphabet[idx - shift]
+    print(decoded)
+  decrypt(message, shift_value)
+
+else:
+  print('Enter encode or decode')
