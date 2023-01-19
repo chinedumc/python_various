4319ff1658ecd7ec029d3e2d8775c69cd5dca505
chibokamc <chibokachinedum@gmail.com>
2023-01-19 23:42:35 +0100

    Improved user experience: Added option to continue or quit the game, Added use of large shift values

--- 4-caesarCipher.py
+++ 4-caesarCipher.py
@@ -1,13 +1,10 @@
 import string
 a = string.ascii_lowercase
 print(a)
-alphabet = [*a]
-print(alphabet + alphabet)
+alpha = [*a]
+alphabet=alpha + alpha
+print(alphabet)
 
-
-message = input("Type your message:\n").lower()
-shift_value = int(input("Enter a shift value:\n"))
-encode_decode = input("Type 'encode' or 'decode':\n").lower()
 
 # if(encode_decode == 'encode'):
 #   def encrypt(text, shift):
@@ -30,9 +27,12 @@ encode_decode = input("Type 'encode' or 'decode':\n").lower()
 # else:
 #   print('Enter encode or decode')
 
+end_game = False
+
+
 def encrypt_decrypt(text, shift, encode_decode):
   if encode_decode == 'encode':
-    shift = shift*1
+    shift *= 1
   elif encode_decode == 'decode':
     shift *= -1
   else:
@@ -42,4 +42,19 @@ def encrypt_decrypt(text, shift, encode_decode):
     idx = alphabet.index(letter)
     coded_decoded += alphabet[idx + shift]
   print(coded_decoded)
-encrypt_decrypt(message, shift_value, encode_decode)
+
+while not end_game:
+
+  message = input("Type your message:\n").lower()
+  shift_value = int(input("Enter a shift value:\n"))
+  encode_decode = input("Type 'encode' or 'decode':\n").lower()
+
+  shift_value = shift_value % 26
+
+  encrypt_decrypt(text=message, shift=shift_value, encode_decode=encode_decode)
+
+  new_round = input("Type 'yes' if you want to decode/encode again. Or type 'no' to quit\n").lower()
+
+  if new_round == 'no':
+    end_game = True
+    print('You have exited the game')
